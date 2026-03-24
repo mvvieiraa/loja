b439f6bcf8078747956f3c166d21be41d08b3746
milena vieira <milenasantos09070182@gmail.com>
2026-03-24 08:28:46 -0300

    Finalizando o Header

---
+++ src/components/Topo/index.jsx
@@ -0,0 +1,28 @@
+import './Topo.css'
+
+export default function Topo() {
+    return (
+        <header>
+            <div className='anuncio'>
+                <div className='anuncio-conteudo'>
+                    <p>Tudo em 10x sem juros!</p>
+                </div>
+            </div>
+
+            <div className='topo'>
+                <picture>
+                    <a href='#'>
+                        <img src='./logoLoja.png' />
+                    </a>
+                </picture>
+
+                <nav>
+                    <a href='#'>Home</a>
+                    <a href='#'>Produtos</a>
+                    <a href='#'>Lançamentos</a>
+                    <a href='#'>Contato</a>
+                </nav>
+            </div>
+        </header>
+    )
+}
